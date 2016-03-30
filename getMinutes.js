489306effe0d0cdee802d78216c70fb2187e0498
Rob Halff <rob.halff@gmail.com>
2016-03-30 12:29:55 +0200

    update

--- getMinutes.js
+++ getMinutes.js
@@ -13,7 +13,7 @@ module.exports = {
         title: "Date",
         type: "Date",
         async: true,
-        fn: function __IN__(data, x, source, state, input, output) {
+        fn: function __IN__(data, source, state, input, $, output) {
           var r = function() {
             output({
               out: $.write('in', $.in.getMinutes())
